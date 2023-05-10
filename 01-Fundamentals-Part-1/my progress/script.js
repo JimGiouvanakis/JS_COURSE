0e7451f27045e1336676ee185941dd6ab74d757d
Giouvanakis <dgiouvanakis@deloitte.gr>
2023-05-10 17:56:57 +0300

    Some Progress

---
+++ 01-Fundamentals-Part-1/my progress/script.js
@@ -0,0 +1,26 @@
+// let js = "amazing";
+// // console.log(40 + 8 + 23 - 10);
+
+// console.log("Jonas");
+// console.log(23);
+
+// let firstName = "Matilda";
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "jonas");
+
+javascriptIsFun = "yes";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
