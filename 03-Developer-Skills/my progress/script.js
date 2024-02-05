0f70c7655e0d8bacc79a2c5d2db9a0361796923a
JimGiouvanakis <106709373+JimGiouvanakis@users.noreply.github.com>
2024-02-05 19:44:15 +0200

    Progress

--- 03-Developer-Skills/my progress/script.js
+++ 03-Developer-Skills/my progress/script.js
@@ -1,10 +1,57 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
-const x = "23";
+// const x = "23";
 
-if (x === 23) console.log(23);
+// if (x === 23) console.log(23);
 
-const calcAge = (birthYear) => 2037 - birthYear;
+// const calcAge = (birthYear) => 2037 - birthYear;
 
-console.log(calcAge(1998));
+// console.log(calcAge(1998));
+
+// const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+// let min, max;
+// function RemoveErrors(temp) {
+//   for (let i = 0; i < temp.length; i++) {
+//     if (temp[i] === "error") {
+//       temp.splice(i, 1);
+//     }
+//   }
+//   return temp;
+// }
+
+// function MinMax(temp, min, max) {
+//   max = Math.max(...temp);
+//   min = Math.min(...temp);
+//   return { min: min, max: max };
+// }
+
+// let checked = RemoveErrors(temperatures);
+// let result = MinMax(checked, min, max);
+// console.log(result.min, result.max);
+
+// const measureKelvin = function () {
+//   const measurment = {
+//     type: "temp",
+//     unit: `celsius`,
+//     value: Number(prompt(`Degress celsius:`)),
+//   };
+//   console.table(measurment);
+
+//   const kelvin = measurment.value + 273;
+//   return kelvin;
+// };
+
+// console.log(measureKelvin());
+
+const MaxTemp = [17, 21, 23, 12, 5, -5, 0, 4];
+
+function PrintForecast(MaxTemp) {
+  let str = "";
+  for (let i = 0; i < MaxTemp.length; i++) {
+    str = str + `..${MaxTemp[i]}°C in ${i + 1} days`;
+  }
+  return str;
+}
+
+console.log(PrintForecast(MaxTemp));
--- 05-Guess-My-Number/my progress/script.js
+++ 05-Guess-My-Number/my progress/script.js
@@ -1,1 +1,51 @@
-'use strict';
+"use strict";
+
+// console.log(document.querySelector(".message").textContent);
+
+// document.querySelector(`.message`).textContent = "Correct! :D";
+
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 25;
+
+// document.querySelector(`.guess`).value;
+
+let secretnumber = Math.floor(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector(".number").textContent = secretnumber;
+
+document.querySelector(`.check`).addEventListener("click", function () {
+  const guess = Number(document.querySelector(`.guess`).value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "Give a Number!";
+  } else {
+    if (secretnumber > guess) {
+      if (score > 1) {
+        document.querySelector(".message").textContent = "Too Low";
+        score--;
+        document.querySelector(".score").textContent = score;
+      } else {
+        document.querySelector(".message").textContent = "You Lose the Game";
+        document.querySelector(".score").textContent = 0;
+      }
+    } else if (secretnumber < guess) {
+      if (score > 1) {
+        document.querySelector(".message").textContent = "Too High";
+        score--;
+        document.querySelector(".score").textContent = score;
+      } else {
+        document.querySelector(".message").textContent = "You Lose the Game";
+        document.querySelector(".score").textContent = 0;
+      }
+    } else if (secretnumber === guess) {
+      document.querySelector(".message").textContent = "Correct Number!";
+      let highscore = document.querySelector(".highscore").textContent;
+      console.log(document.querySelector(".highscore").textContent);
+
+      if (highscore > score) {
+        document.querySelector(".highscore ").textContent = score;
+      }
+    }
+  }
+});
